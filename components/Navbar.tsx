cecedc54be7f6ff7d209b7269f81caace9b36cff
rweventos <rweventos.rj@gmail.com>
2023-09-18 06:14:49 -0300

    minor changes on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,24 +5,23 @@ import Link from "next/link";
 export default async function Navbar() {
   const user = await getCurrentUser();
   return (
-    <nav className="bg-zinc-700 px-4 flex justify-between items-center">
+    <nav className="sticky  top-0 left-0 border-b border-b-black/5 drop-shadow-sm z-20  px-4 flex justify-between items-center backdrop-blur-sm ">
       <Link href="/">
-        <Image alt="logo" src="/ecoHouse.svg" height={60} width={60} />
+        <Image
+          alt="logo"
+          src="/ecoHouse.svg"
+          height={40}
+          width={40}
+          className="opacity-100"
+        />
       </Link>
-      {/* <ul className="flex  text-base font-medium text-neutral-200 space-x-4">
-        <li>
-          <Link href="/api/auth/signin">Sign In</Link>
-        </li>
-        <li>
-          <Link href="/api/auth/signout">Sign Out</Link>
-        </li>
-      </ul> */}
+
       {user ? (
         <div className="flex flex-col items-center rounded-full font-bold text-3xl text-neutral-100">
           <Image src={user?.image} alt="user" height={60} width={60} />
         </div>
       ) : (
-        <div className="flex flex-col items-center rounded-full font-bold text-md text-neutral-100">
+        <div className="flex flex-col items-center rounded-full font-bold text-md text-slate-800">
           <Link href="/login">Login</Link>
         </div>
       )}
